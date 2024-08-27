f0e3296a91cfa8041b8d8fdd801397cd782ee5e2
Nguyễn Hoàng Minh 79 (AI) <minhnh79@fpt.com.vn>
2024-08-27 17:32:29 +0700

    add test routes, api

---
+++ leetcode/1514/main.go
@@ -0,0 +1,17 @@
+package main
+
+// You are given an undirected weighted graph of n nodes (0-indexed), represented by an edge list where edges[i] = [a, b] is an undirected edge connecting the nodes a and b with a probability of success of traversing that edge succProb[i].
+
+// Given two nodes start and end, find the path with the maximum probability of success to go from start to end and return its success probability.
+
+// If there is no path from start to end, return 0. Your answer will be accepted if it differs from the correct answer by at most 1e-5
+
+// Input: n = 3, edges = [[0,1],[1,2],[0,2]], succProb = [0.5,0.5,0.2], start = 0, end = 2
+// Output: 0.25000
+// Explanation: There are two paths from start to end, one having a probability of success = 0.2 and the other has 0.5 * 0.5 = 0.25.
+
+func maxProbability(n int, edges [][]int, succProb []float64, start_node int, end_node int) float64 {
+	return float64(12)
+}
+
+func main() {}
